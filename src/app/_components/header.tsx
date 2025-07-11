d8515528779ba0425e5d6981830e99bb2edaf94b
Gustavo Cunha <gustavofaccocunha@gmail.com>
2025-07-11 19:49:28 -0300

    feat: Inicio do menu lateral

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,21 +1,98 @@
-import Image from "next/image";
-import { Card, CardContent } from "./ui/card";
-import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
+import Image from "next/image"
+import { Card, CardContent } from "./ui/card"
+import { Button } from "./ui/button"
+import { CalendarIcon, HomeIcon, LogOutIcon, MenuIcon } from "lucide-react"
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet"
+import { quickSearchOptions } from "../_constants/search"
+import { Avatar, AvatarImage } from "./ui/avatar"
+import Link from "next/link"
 
 const Header = () => {
-    return (
-   <div>
-     <Card className="rounded-none">
-        <CardContent className="p-5 flex flex-row items-center justify-between">
-            <Image alt="FSW-Barber" src="/logo.png" height={18} width={120}/>
-            <Button size="icon" variant="outline">
+  return (
+    <div>
+      <Card className="rounded-none">
+        <CardContent className="flex flex-row items-center justify-between p-5">
+          <Image alt="FSW-Barber" src="/logo.png" height={18} width={120} />
+
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button size="icon" variant="outline">
                 <MenuIcon />
-            </Button>
+              </Button>
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle className="text-left">Menu</SheetTitle>
+              </SheetHeader>
+
+              <div className="flex items-center gap-3 border-b border-solid py-5">
+                <Avatar>
+                  <AvatarImage src="https://avatars.githubusercontent.com/u/111590688?v=4" />
+                </Avatar>
+
+                <div>
+                  <p className="font-bold">Gustavo Facco Cunha</p>
+                  <p className="text-sm">gustavofaccocunha@gmail.com</p>
+                </div>
+              </div>
+
+              <div className="flex flex-col gap-2 border-b border-solid py-5">
+                <SheetClose asChild>
+                  <Button
+                    className="justify-start gap-2"
+                    variant="ghost"
+                    asChild
+                  >
+                    <Link href="/">
+                      <HomeIcon size={18} />
+                      Inicio
+                    </Link>
+                  </Button>
+                </SheetClose>
+                <Button className="justify-start gap-2" variant="ghost">
+                  <CalendarIcon size={18} />
+                  Agendamentos
+                </Button>
+              </div>
+
+              <div className="flex flex-col gap-2 border-b border-solid py-5">
+                {quickSearchOptions.map((option) => (
+                  <Button
+                    className="justify-start gap-2"
+                    variant="ghost"
+                    key={option.title}
+                  >
+                    <Image
+                      alt={option.title}
+                      src={option.imageUrl}
+                      height={18}
+                      width={18}
+                      key={option.title}
+                    />
+                    {option.title}
+                  </Button>
+                ))}
+              </div>
+
+              <div className="flex flex-col gap-2 py-5">
+                <Button className="justify-start gap-2" variant="ghost">
+                  <LogOutIcon size={18} />
+                  Sair da Conta
+                </Button>
+              </div>
+            </SheetContent>
+          </Sheet>
         </CardContent>
-    </Card>
-   </div>
-    );
+      </Card>
+    </div>
+  )
 }
- 
-export default Header;
+
+export default Header
